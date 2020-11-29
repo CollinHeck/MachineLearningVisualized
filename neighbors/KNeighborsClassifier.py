9cd5f6933a6ffa08428cce127524ed3e99daeb4b
BuildTools <collinheck@gmail.com>
2020-11-29 03:31:55 -0500

    Added first version of KNN, but needs improvement listed in code TODOs

--- neighbors/KNeighborsClassifier.py
+++ neighbors/KNeighborsClassifier.py
@@ -1,1 +1,36 @@
-# TODO: Implement a KNN Classifier
+import numpy as np
+
+
+class KNeighborsClassifier():
+
+    # TODO: Add classification info
+    def __init__(self, X=None, k=3):
+        self.X = X
+        self.k = k
+
+    def fit(self, X):
+        self.X = X
+
+    def setK(self, k):
+        self.k = k
+
+    def predict(self, y):
+        # TODO: Fix the outputs
+
+        distances = []
+        rows = []
+        counter = 0
+        for row in self.X:
+            distances.append(self.euclideanDistance(row, y))
+            rows.append(counter)
+            counter += 1
+
+        out = zip(distances, rows)
+        print(tuple(out))
+
+    @staticmethod
+    def euclideanDistance(arr1, arr2):
+        assert arr1.shape == arr2.shape, "Arrays need to be equal shapes for Euclidean Distance."
+        return np.sqrt(np.sum(np.square(arr1 - arr2)))
+
+    # TODO: Add visuals
--- neighbors/KNeighborsClassifierTest.py
+++ neighbors/KNeighborsClassifierTest.py
@@ -1,1 +1,37 @@
-# TODO: Temporarily test KNN class during development.
+# Temporarily test KNN class during development.
+
+
+import numpy as np
+
+from KNeighborsClassifier import *
+
+knn = KNeighborsClassifier()
+
+a = np.array([1, 2, 3, 4, 5])
+b = np.array([5, 6, 7, 8, 9])
+
+e = knn.euclideanDistance(a,b)
+print(e)
+
+
+fitA = np.array([[4, 3, 3, 4, 9],
+                 [1, 2, 3, 4, 5],
+                 [5, 6, 7, 8, 9],
+                 [0, 0, 3, 4, 6],
+                 [11, 22, 34, 45, 63],
+                 [5, 4, 3, 2, 100],
+                 [1, 2, 3, 4, 1540000]])
+
+knn.fit(fitA)
+knn.setK(3)
+
+knn.predict(a)
+
+print('Next')
+
+knn.predict(b)
+
+print('Next')
+
+print(fitA.shape)
+print(np.arange(0,fitA.shape[0]))
